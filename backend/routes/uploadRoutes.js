dec9b36aa9561fa7d9f814e01d28e447dd1adb95
Yaniv Kabariti <kabariti2@gmail.com>
2024-05-29 19:34:10 +0300

    fix uploads dir

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, `${path.resolve()}/uploads/`)
+        cb(null, 'uploads/')
     },
     filename(req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
